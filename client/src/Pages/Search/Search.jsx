d0f418f1d8d752afb361dcd8d2988c2ca9dc3268
Kenny Lam <lamkenny1010@gmail.com>
2023-09-25 18:20:51 +1300

    Implement search feature

--- client/src/Pages/Search/Search.jsx
+++ client/src/Pages/Search/Search.jsx
@@ -4,82 +4,122 @@ import SearchBar from "@mkyy/mui-search-bar";
 import MediaGrid from "../../Components/MediaGrid";
 import "./style.css";
 import { apiKey, BASE_URL } from "../../../env.js";
-import backgroundImage from "./img/movies.jpeg"; 
-
-
+import backgroundImage from "./img/movies.jpeg";
+import { useAuthContext } from "../../Hooks/useAuthContext";
+import { addFriend } from "../../Functions/addFriend";
+import { declineFriendRequest } from "../../Functions/declineFriendRequest.js";
+import { acceptFriendRequest } from "../../Functions/acceptFriendRequest";
+import { useFriendRequests } from "../../Hooks/useFriendRequests";
+import { useFriends } from "../../Hooks/useFriends";
+import { useSearchFriends } from "../../Hooks/useSearchUsers";
 const Search = () => {
-    const [backendData, setBackendData] = useState({ movies: [] });
     const [textFieldValue, setTextFieldValue] = useState(""); // Start with an empty search query
-    const [filteredMovies, setFilteredMovies] = useState([]);
-    const [isInitialLoad, setIsInitialLoad] = useState(true); // Track the initial load
+    const [reload, setReload] = useState(false);
+    const { user } = useAuthContext();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const query =
-                    isInitialLoad || textFieldValue.trim() === ""
-                        ? "a"
-                        : textFieldValue.trim();
-                const response = await axios.get(
-                    `${BASE_URL}/search/movie?api_key=${apiKey}&query=${query}`
-                );
-                if (response.status === 200) {
-                    setBackendData({ movies: response.data.results });
-                } else {
-                    console.error("Request failed with status:", response.status);
-                }
-            } catch (error) {
-                console.error("An error occurred:", error);
-            }
-        };
+    const {
+        friendRequests,
+        isPending: isPendingFriendRequests,
+        error: errorFriendRequests,
+    } = useFriendRequests(user.username, reload);
 
-        fetchData();
-    }, [textFieldValue, isInitialLoad]);
+    const {
+        friends,
+        isPending: isPendingFriends,
+        error: errorFriends,
+    } = useFriends(user.username, reload);
 
-    useEffect(() => {
-        // Filter movies based on textFieldValue
-        const filtered = backendData.movies.filter(
-            (movie) =>
-                movie.title.toLowerCase().includes(textFieldValue.toLowerCase())
-        );
-        setFilteredMovies(filtered);
-    }, [textFieldValue, backendData.movies]);
+    const {
+        searchUsers,
+        isPending: isPendingSearchUsers,
+        error: errorSearchUsers,
+    } = useSearchFriends(textFieldValue);
 
-    useEffect(() => {
-        // After the initial load, set isInitialLoad to false
-        if (isInitialLoad) {
-            setIsInitialLoad(false);
-        }
-    }, [isInitialLoad]);
+    const handleReload = () => {
+        setReload(!reload); // Toggle the reload state to trigger re-fetching
+    };
 
     return (
-        <div className="home-container-search">
-        <div className="background-image-search" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: "cover", backgroundRepeat: "no-repeat" }}></div>
-        <div className="overlay-search"></div>  
-        {/* <div className="page"> */}
+        <div>
             <SearchBar
-                style={{
-                    color: "black",
-                    backgroundColor: "white",
-                    borderRadius: "8px",
-                    border: "1px solid #ccc",
-                    position: "absolute",
-                    top: "20px",
-                    left: "50%",
-                    transform: "translateX(-50%)",
-                    zIndex: "999",
-                }}
                 value={textFieldValue}
                 onChange={(newValue) => {
                     setTextFieldValue(newValue);
                 }}
                 placeholder="Search Movie"
             />
-            
-            <div className="movie-list">
-                <MediaGrid medias={filteredMovies} />
-            {/* </div> */}
-        </div>
+            <div>
+                <div>Friend list</div>
+                <button onClick={handleReload}>Friend list</button>
+                <div>
+                    {errorFriends && <div>{errorFriends}</div>}
+                    {isPendingFriends && <div>Loading...</div>}
+                    {friends &&
+                        friends.map((friend) => {
+                            return (
+                                <>
+                                    <div> {friend}</div>
+                                </>
+                            );
+                        })}
+                </div>
+                <div>Friend requests list</div>
+                <button onClick={handleReload}>Friend Request</button>
+                <div>
+                    {errorFriendRequests && <div>{errorFriendRequests}</div>}
+                    {isPendingFriendRequests && <div>Loading...</div>}
+                    {friendRequests &&
+                        friendRequests.map((friendRequest) => {
+                            return (
+                                <>
+                                    <div> {friendRequest}</div>
+                                    <button
+                                        onClick={() =>
+                                            acceptFriendRequest(
+                                                user.username,
+                                                friendRequest
+                                            )
+                                        }
+                                    >
+                                        Accept
+                                    </button>
+                                    <button
+                                        onClick={() =>
+                                            declineFriendRequest(
+                                                user.username,
+                                                friendRequest
+                                            )
+                                        }
+                                    >
+                                        Decline
+                                    </button>
+                                </>
+                            );
+                        })}
+                </div>
+                <div>Users</div>
+                <div>Hellow</div>
+                {errorSearchUsers && <div>{errorSearchUsers}</div>}
+                {isPendingSearchUsers && <div>Loading...</div>}
+                {searchUsers &&
+                    searchUsers.map((friend) => {
+                        return (
+                            <>
+                                <div> {friend.username}</div>
+                                <button
+                                    onClick={() => {
+                                        addFriend(
+                                            user.username,
+                                            friend.username
+                                        );
+                                    }}
+                                >
+                                    add
+                                </button>
+                            </>
+                        );
+                    })}
+            </div>
         </div>
     );
 };
